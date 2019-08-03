f7163091d76b057d5fa621b6ec045423d3afa18a
CSR_LK <53016847+CSRLK@users.noreply.github.com>
2019-08-03 18:56:14 +0200

    Clase dia antes del parcial

---
+++ Clase_JavaSE/src/clase_javase/Calculadora.java
@@ -0,0 +1,37 @@
+package clase_javase;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author CSR LK
+ */
+public class Calculadora {//NOMBRE DE LA CLASE 
+    
+    public String nombreDeClase; 
+    private int resultadoSuma;
+    
+    //METODO PUBLICO SUMAR 
+    public int sumar(int primerNumero, int segundoNumero)
+    {
+        this.resultadoSuma = primerNumero + segundoNumero;
+        return this.resultadoSuma;
+        
+    
+    }
+    
+    private int restar (int primerNumero, int segundoNuemero)
+    {
+        return 0;
+      
+    
+    
+    }
+
+    
+       
+}
---
+++ Clase_JavaSE/src/clase_javase/Clase_JavaSE.java
@@ -0,0 +1,16 @@
+
+package clase_javase;
+
+public class Clase_JavaSE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Calculadora calculadora = new Calculadora();
+        System.out.println("EL RESULTADO DE LA SUMA ES: "+ calculadora.sumar(10, 5));
+       
+    }
+    
+}
